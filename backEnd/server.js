e67f9c00f89ac41583045cea53af162eab376f31
Esdras <esdraslucena@hotmail.com>
2025-06-06 10:19:25 -0300

    Terceiro e último commit, subida do banco PostgreSQL e funcionamento com prisma

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import cors from 'cors';
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
+const prisma = new PrismaClient();
+
 app.use(cors());
 app.use(express.json());
 
-app.post('/calculate', (req, res) => {
+app.post('/calculate', async (req, res) => {
   const { num1, num2, operator } = req.body;
   let result;
 
@@ -26,8 +29,27 @@ app.post('/calculate', (req, res) => {
       result = 'Operador inválido';
   }
 
+  const expression = `${num1} ${operator} ${num2}`;
+
+  await prisma.calculation.create({
+    data: {
+      expression,
+      result: result.toString(),
+    },
+  });
+
+
   res.json({ result });
 });
+
+app.get('/history', async (req, res) => {
+  const history = await prisma.calculation.findMany({
+    orderBy: { createdAt: 'desc' },
+    take: 10
+  });
+  res.json(history);
+});
+
 
 app.listen(3001, () => {
   console.log('Servidor rodando na porta 3001');
